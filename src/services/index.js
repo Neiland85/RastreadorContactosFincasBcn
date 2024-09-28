2f3be3d9f6981a4f39d5794e8dabd65bed3fb0a7
Neiland85 <theia3impact@gmail.com>
2024-09-29 01:07:29 +0200

    Añadido servicio de OpenAI y filtro de contactos

--- src/services/index.js
+++ src/services/index.js
@@ -1,8 +1,21 @@
 const express = require('express'); 
 const app = express(); 
+const { filtrarContactos } = require('./openaiService');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.send('Hola, servidor en ejecución!');
+});
+
+app.post('/filtrar', async (req, res) => {
+    try {
+        const resultado = await filtrarContactos(req.body.respuestas);
+        res.send({ resultado });
+    } catch (error) {
+        res.status(500).send({ error: 'Error al filtrar contactos' });
+    }
 });
 
 app.listen(3000, () => {
