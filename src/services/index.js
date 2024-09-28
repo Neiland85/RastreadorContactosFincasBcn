bf7a76f824adc11ddfefe5864a6fac450114f073
Neiland85 <theia3impact@gmail.com>
2024-09-29 00:58:42 +0200

    Instalado nodemon y corregido error de puerto ocupado

--- src/services/index.js
+++ src/services/index.js
@@ -1,14 +1,11 @@
-const express = require('express');
-5. const app = express();
-6.
-7. app.use(express.json());
-8.
-9. app.post('/filtrar-contactos', (req, res) => {
-10. const { respuestas } = req.body;
-11. // Aquí integrarías la IA para filtrar los contactos
-12. res.json({ mensaje: 'Contacto procesado' });
-13. });
-14.
-15. app.listen(3000, () => {
-16. console.log('Servidor escuchando en puerto 3000');
-17. });
+const express = require('express'); 
+const app = express(); 
+
+app.get('/', (req, res) => {
+    res.send('Hola, servidor en ejecución!');
+});
+
+app.listen(3000, () => {
+    console.log('Servidor escuchando en el puerto 3000');
+});
+
